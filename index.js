357b9bf9d561a7cd6855060ec1bd41141171d3f3
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-04-17 03:32:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,13 @@ class ContaCorrente{
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.rg = 123456789;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 console.log(cliente1);
 console.log(cliente2);
