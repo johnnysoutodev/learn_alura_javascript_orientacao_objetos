b97713863684152efed674724b2ef0b5612c0505
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-04-17 04:06:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    saldo;
+    // #saldo = 0
+    _saldo = 0;
 
     sacar(valor){
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
         }
     }
 
     depostiar(valor){
         if (valor > 0) {
-            this.saldo += valor;
+            this._saldo += valor;
         }
     }
 }
@@ -31,10 +32,9 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
 contaCorrenteRicardo.depostiar(1000);
 contaCorrenteRicardo.sacar(20);
 
-console.log(contaCorrenteRicardo.saldo);
+console.log(contaCorrenteRicardo);
